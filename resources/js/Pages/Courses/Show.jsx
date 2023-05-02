f30600726a3f3c22aa1cc2fc81a1f689fe7cdaf8
ricardoscarpim <rscarpimbra@hotmail.com>
2023-05-02 14:58:50 -0400

    Show Courses remove skelleton

--- resources/js/Pages/Courses/Show.jsx
+++ resources/js/Pages/Courses/Show.jsx
@@ -32,29 +32,6 @@ export default function Show({ auth, language }) {
 
 
 
-    /*  */
-    if(!getData.length) return (
-        <>
-            <section className="bg-white dark:bg-gray-900">
-                <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-                    <div className="max-w-screen-sm text-left mb-8 lg:mb-16">
-                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{ t('sub title') }</h2>
-                        <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">{ t('sub title description')}</p>
-                    </div>
-
-
-                    {/* Creating the List of Courses. */}
-                    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-
-                        {/* Call a Auto Generate Component to Generate the Sections. */}
-                        { Skeleton({Number: 4}) }
-                   </div>
-                </div>
-            </section>
-        </>
-    )
-
-
 
     return (
         <AuthenticatedLayout
